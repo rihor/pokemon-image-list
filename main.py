12e010c730b63121b9bfa5073c1800e9140db96f
Pedro Pinho <rihor1000@gmail.com>
2020-03-04 13:46:04 -0300

    Removed limit and refactored code
    Now it doesn't need to now how many pokemons are there in the PokemonAPI

--- main.py
+++ main.py
@@ -1,37 +1,37 @@
 """
-Code that links images of pokemon from the official website with their
-names and ids and generates a JSON file
+Code that takes the results of PokemonAPI and links
+with their official images and generates a JSON file
 """
 import json
 import requests
 
-NUM_OF_POKEMONS = 808
 POKEMONS = []
 
-for i in range(1, NUM_OF_POKEMONS):
-    rs_pokemon = requests.get(f"https://pokeapi.co/api/v2/pokemon/{i}")
-    pokemon = rs_pokemon.json()
+ITERATOR = 1
 
-    p_id = str(pokemon["id"]).zfill(3)
-    p_name = pokemon["name"]
+while True:
+    RS_POKEMON = requests.get(f"https://pokeapi.co/api/v2/pokemon/{ITERATOR}")
 
-    rs_image_high = (
-        f"https://assets.pokemon.com/assets/cms2/img/pokedex/full/{p_id}.png"
-    )
-    rs_image_small = (
-        f"https://assets.pokemon.com/assets/cms2/img/pokedex/detail/{p_id}.png"
-    )
+    if RS_POKEMON.status_code != 200:
+        print("No more pokemons!")
+        break
+
+    POKEMON = RS_POKEMON.json()
+
+    P_ID = str(POKEMON["id"]).zfill(3)
+    P_NAME = POKEMON["name"]
 
     POKEMONS.append(
         {
-            "id": p_id,
-            "name": p_name,
-            "image_hq": rs_image_high,
-            "image": rs_image_small,
+            "id": P_ID,
+            "name": P_NAME,
+            "image_hq": f"https://assets.pokemon.com/assets/cms2/img/pokedex/full/{P_ID}.png",
+            "image": f"https://assets.pokemon.com/assets/cms2/img/pokedex/detail/{P_ID}.png",
         }
     )
 
-    print(f"id:{p_id} name:{p_name} succefully added!")
+    print(f"id:{P_ID} name:{P_NAME} succefully added!")
+    ITERATOR += 1
 
 
 with open("pokemons.json", "w") as outfile:
